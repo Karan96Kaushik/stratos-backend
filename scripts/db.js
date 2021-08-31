f6910af2ee266452b24377474169f4ed8aed29d8
KaranShyplite <karan@shyplite.com>
2021-08-31 22:41:29 +0530

    db proc env

--- scripts/db.js
+++ scripts/db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 const {Clients} = require("../models/Clients");
 const crypto = require("crypto");
 const {Utils} = require("../models/Utils");
+
+// 'mongodb://karan:karan12345@bayonetbaron.com:27017/tms?authSource=admin'
+const dbCreds = process.env.dbCreds
+
 mongoose
-	.connect('mongodb://karan:karan12345@bayonetbaron.com:27017/tms?authSource=admin', {
+	.connect(dbCreds, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: false
