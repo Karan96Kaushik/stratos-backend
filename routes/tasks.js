48f1486ad502e1724eeb8345520bb70d654d3132
KaranShyplite <karan@shyplite.com>
2021-09-27 12:22:18 +0530

    task update fix

--- routes/tasks.js
+++ routes/tasks.js
@@ -564,7 +564,7 @@ router.post("/api/tasks/update", async (req, res) => {
 
 		req.body.totalAmount = calculateTotal(req.body)
 
-		if(task.addedBy != req.user.id && !task._membersAssigned.includes(String(req.user.id)) && !adminIDs.includes(req.user.id)) {
+		if(task.addedBy != req.user.id && !(task._membersAssigned ?? []).includes(String(req.user.id)) && !adminIDs.includes(req.user.id)) {
 			res.status(401).send("Unauthorized to update this task")
 			return
 		}
