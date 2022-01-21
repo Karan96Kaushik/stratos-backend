a54cd5622cca49c8d810194cb246bb5489367551
Karan <karankshk96@gmail.com>
2022-01-21 18:14:24 +0530

    invoice pdf update

--- tests/mailer2.js
+++ tests/mailer2.js
@@ -6,7 +6,7 @@ let f2 = `
 		<tr>
 			<td style="height:147px; width:58.561%"><span style="font-family:Leelawadee UI,sans-serif"><img alt="" src="https://user-images.githubusercontent.com/44289074/148599687-1426045b-729f-492e-95c3-b2c5cd0d43e4.png" style="height:79px; width:253px" /></span></td>
 			<td style="height:147px; width:41.439%">
-			<h3><span style="font-family:Leelawadee UI,sans-serif"><strong><u>BILL FROM:</u></strong></span></h3>
+			<h3><span style="font-family:Leelawadee UI,sans-serif"><strong><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red">BILL FROM:</u></strong></span></h3>
 
 			<h4><span style="font-family:Leelawadee UI,sans-serif"><strong>Osha Technologies</strong></span></h4>
 
@@ -15,53 +15,53 @@ let f2 = `
 		</tr>
 		<tr>
 			<td style="height:128px; width:58.561%">
-			<h3><span style="font-family:Leelawadee UI,sans-serif"><u><strong>BILL TO:</strong></u></span></h3>
+			<h3><span style="font-family:Leelawadee UI,sans-serif"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><strong>BILL TO:</strong></u></span></h3>
 			<span style="font-family:Leelawadee UI,sans-serif">Maharashtra Developers</span>
 
-			<h3><span style="font-family:Leelawadee UI,sans-serif"><strong><u>PROJECT NAME:</u></strong></span></h3>
+			<h3><span style="font-family:Leelawadee UI,sans-serif"><strong><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red">PROJECT NAME:</u></strong></span></h3>
 			<span style="font-family:Leelawadee UI,sans-serif"> JVMS Corner Stone</span></td>
 			<td style="height:128px; width:41.439%">
 			<table border="0" cellspacing="0" style="border-collapse:collapse; border-style:hidden; height:108px; width:100%">
 				<tbody>
 					<tr>
 						<td style="height:18px; width:50%"><span style="font-family:Leelawadee UI,sans-serif">Type</span></td>
-						<td style="height:18px; width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">Proforma Invoice</span></u></td>
+						<td style="height:18px; width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">Proforma Invoice</span></u></td>
 					</tr>
 					<tr>
 						<td style="height:36px; width:50%"><span style="font-family:Leelawadee UI,sans-serif">Date</span></td>
-						<td style="height:36px; width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">14th Dec 2021</span></u></td>
+						<td style="height:36px; width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">14th Dec 2021</span></u></td>
 					</tr>
 					<tr>
 						<td style="height:18px; width:50%"><span style="font-family:Leelawadee UI,sans-serif">PAN No</span></td>
-						<td style="height:18px; width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">AAFFO8457Q</span></u></td>
+						<td style="height:18px; width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">AAFFO8457Q</span></u></td>
 					</tr>
 					<tr>
 						<td style="height:18px; width:50%"><span style="font-family:Leelawadee UI,sans-serif">Ref No</span></td>
-						<td style="height:18px; width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">RERA001959</span></u></td>
+						<td style="height:18px; width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">RERA001959</span></u></td>
 					</tr>
 					<tr>
 						<td style="height:18px; width:50%"><span style="font-family:Leelawadee UI,sans-serif">GSTIN</span></td>
-						<td style="height:18px; width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">27AAFFO8457Q1ZB</span></u></td>
+						<td style="height:18px; width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">27AAFFO8457Q1ZB</span></u></td>
 					</tr>
 				</tbody>
 			</table>
 			</td>
 		</tr>
 		<tr>
-			<td style="height:110px; width:58.561%"><span style="font-family:Leelawadee UI,sans-serif"><u>Bill Details</u></span>
+			<td style="height:110px; width:58.561%"><span style="font-family:Leelawadee UI,sans-serif"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red">Bill Details</u></span>
 			<table border="0" cellspacing="0" style="border-collapse:collapse; border-style:hidden; width:100%">
 				<tbody>
 					<tr>
 						<td style="width:50%"><span style="font-family:Leelawadee UI,sans-serif">Bill Period:</span></td>
-						<td style="width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">NA</span></u></td>
+						<td style="width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">NA</span></u></td>
 					</tr>
 					<tr>
 						<td style="width:50%"><span style="font-family:Leelawadee UI,sans-serif">Quotation No:</span></td>
-						<td style="width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">NA</span></u></td>
+						<td style="width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">NA</span></u></td>
 					</tr>
 					<tr>
 						<td style="width:50%"><span style="font-family:Leelawadee UI,sans-serif">Quotation Date:</span></td>
-						<td style="width:50%"><u><span style="font-family:Leelawadee UI,sans-serif">NA</span></u></td>
+						<td style="width:50%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">NA</span></u></td>
 					</tr>
 				</tbody>
 			</table>
@@ -127,34 +127,34 @@ let f2 = `
 <table border="0" cellspacing="0" style="border-collapse:collapse; width:100%">
 	<tbody>
 		<tr>
-			<td style="width:56.102%"><u><span style="font-family:Leelawadee UI,sans-serif"><strong>Special Notes</strong></span></u></td>
+			<td style="width:56.102%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif"><strong>Special Notes</strong></span></u></td>
 			<td style="width:24.0436%"><span style="font-family:Leelawadee UI,sans-serif"><strong>SUBTOTAL</strong></span></td>
-			<td style="width:19.8543%"><u><span style="font-family:Leelawadee UI,sans-serif">50000</span></u></td>
+			<td style="width:19.8543%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">50000</span></u></td>
 		</tr>
 		<tr>
 			<td style="width:56.102%"><span style="font-family:Leelawadee UI,sans-serif">NA</span></td>
 			<td style="width:24.0436%"><span style="font-family:Leelawadee UI,sans-serif"><strong>TAX RATE</strong></span></td>
-			<td style="width:19.8543%"><u><span style="font-family:Leelawadee UI,sans-serif">18%</span></u></td>
+			<td style="width:19.8543%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">18%</span></u></td>
 		</tr>
 		<tr>
 			<td style="width:56.102%">&nbsp;</td>
 			<td style="width:24.0436%"><span style="font-family:Leelawadee UI,sans-serif"><strong>TOTAL TAX</strong></span></td>
-			<td style="width:19.8543%"><u><span style="font-family:Leelawadee UI,sans-serif">9000</span></u></td>
+			<td style="width:19.8543%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">9000</span></u></td>
 		</tr>
 		<tr>
 			<td style="width:56.102%">&nbsp;</td>
 			<td style="width:24.0436%"><span style="font-family:Leelawadee UI,sans-serif"><strong>TOTAL AMOUNT</strong></span></td>
-			<td style="width:19.8543%"><u><span style="font-family:Leelawadee UI,sans-serif">59000</span></u></td>
+			<td style="width:19.8543%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">59000</span></u></td>
 		</tr>
 		<tr>
 			<td style="width:56.102%">&nbsp;</td>
 			<td style="width:24.0436%"><span style="font-family:Leelawadee UI,sans-serif"><strong>PAYMENT MADE</strong></span></td>
-			<td style="width:19.8543%"><u><span style="font-family:Leelawadee UI,sans-serif">0</span></u></td>
+			<td style="width:19.8543%"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><span style="font-family:Leelawadee UI,sans-serif">0</span></u></td>
 		</tr>
 		<tr>
 			<td style="width:56.102%">&nbsp;</td>
 			<td style="width:24.0436%"><span style="font-family:Leelawadee UI,sans-serif"><strong>BALANCE PAYABLE</strong></span></td>
-			<td style="width:19.8543%"><span style="font-family:Leelawadee UI,sans-serif"><u><strong>59000</strong></u></span></td>
+			<td style="width:19.8543%"><span style="font-family:Leelawadee UI,sans-serif"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><strong>59000</strong></u></span></td>
 		</tr>
 	</tbody>
 </table>
@@ -166,7 +166,7 @@ let f2 = `
 <table border="0" cellspacing="0" style="border-collapse:collapse; height:108px; width:100%">
 	<tbody>
 		<tr>
-			<td style="height:18px; width:51.0018%"><span style="font-family:Leelawadee UI,sans-serif"><u><strong>PAYMENT INFORMATION</strong></u></span></td>
+			<td style="height:18px; width:51.0018%"><span style="font-family:Leelawadee UI,sans-serif"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><strong>PAYMENT INFORMATION</strong></u></span></td>
 			<td style="height:18px; width:25.8652%">&nbsp;</td>
 			<td style="height:18px; width:22.9508%">&nbsp;</td>
 		</tr>
@@ -176,7 +176,7 @@ let f2 = `
 			<td style="height:18px; width:22.9508%">&nbsp;</td>
 		</tr>
 		<tr>
-			<td style="height:18px; width:51.0018%"><span style="font-family:Leelawadee UI,sans-serif"><u><strong>For NEFT/RTGS/IMPS:-</strong></u></span></td>
+			<td style="height:18px; width:51.0018%"><span style="font-family:Leelawadee UI,sans-serif"><u style="text-decoration:none;box-shadow: inset 0 0px 0 white, inset 0 -1px 0 red"><strong>For NEFT/RTGS/IMPS:-</strong></u></span></td>
 			<td style="height:18px; width:25.8652%">&nbsp;</td>
 			<td style="height:18px; width:22.9508%">&nbsp;</td>
 		</tr>
