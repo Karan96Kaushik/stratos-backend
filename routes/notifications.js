4c37a8ff153d06f5d466a3579079390398cfe0e8
Karan <karankshk96@gmail.com>
2023-11-08 05:22:51 +0000

    cached notifs

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,15 +1,17 @@
-const fs = require('fs');
-const crypto = require("crypto");
 const moment = require("moment");
-const mongoose = require("mongoose");
 const router     = new (require('express')).Router()
 
 const {Notifications} = require("../models/Notifications");
+const client = require('../scripts/redisClient');
 
 router.get("/api/notifications", async (req, res) => {
 	try {
 		let _;
 
+		const lastNotif = await getLastNotificationTime(req.query.mid)
+		if ( req.query.useCached && (!lastNotif || lastNotif < (+new Date - 10000)) )
+			return res.status(304).send('ok')
+		
 		let data = await Notifications.find({
 			_memberID: req.query.mid
 		})
@@ -27,5 +29,16 @@ router.get("/api/notifications", async (req, res) => {
 		res.status(500).send(err.message)
 	}
 })
+
+const getLastNotificationTime = async (memberID) => {
+    try {
+        // console.debug(String(_memberID), String(ticketID))
+        const msgTime = await client.hGet('NOTIF', String(memberID))
+        return Number(msgTime)
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
 
 module.exports = router
