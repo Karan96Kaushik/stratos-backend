1fb5ea16b4c1b9decbae33cd46af1a220356315a
Karan96Kaushik <karankshk96@gmail.com>
2024-05-15 22:36:50 +0100

    sales export fileds

--- statics/salesFields.js
+++ statics/salesFields.js
@@ -21,7 +21,10 @@ const salesFields = {
             {label:"Rating", id:"rating", options:[1,2,3,4,5]},
             {label:"Remarks", id:"remarks", isHidden:false},           
             {label:"FollowUp Date", id:"followUpDate", type:"date"},
+            {label:"Calling Date", id:"callingDate", type:"date", isRequired: true},
             {label:"Meeting Date", id:"meetingDate", type:"date"},
+            {label:"Quoted Amount", id:"quotedAmount", type:"number"},
+            {label:"Confirmed Amount", id:"confirmedAmount", type:"number"},
         ],
         checkboxes:[
         ]
