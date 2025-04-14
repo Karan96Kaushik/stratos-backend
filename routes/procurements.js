6f14f37d8579f0746aa931056e04962628297c1b
Karan96Kaushik <karankshk96@gmail.com>
2025-04-14 22:44:33 +0100

    minor

--- routes/procurements.js
+++ routes/procurements.js
@@ -130,6 +130,7 @@ const commonProcessor = (results) => {
 	results = results.map(val => ({
 		...val, 
 		createdTime:moment(new Date(val.createdTime)).format("DD-MM-YYYY"),
+		billDate:moment(new Date(val.billDate)).format("DD-MM-YYYY"),
 		followUpDate: !val.followUpDate ? "" : moment(new Date(val.followUpDate)).format("DD-MM-YYYY"),
         existingRemarks: Array.isArray(val.remarks) ? val.remarks : [val.remarks],
         remarks: ''
